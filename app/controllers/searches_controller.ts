52c511dd535e9fe7eb91673de94b3e8bd61b0c98
brunoverpet <bverpet46@gmail.com>
2025-09-06 13:34:44 +0200

    Update searches_controller.ts

--- app/controllers/searches_controller.ts
+++ app/controllers/searches_controller.ts
@@ -1,15 +1,33 @@
 import { inject } from '@adonisjs/core'
 import { TMDBService } from '#services/tmdb_service'
 import { HttpContext } from '@adonisjs/core/http'
+import WatchedItem from '#models/watched_item'
 
 @inject()
 export default class SearchesController {
   constructor(private tmdbService: TMDBService) {}
 
-  async searchItem({ request, response }: HttpContext) {
+  async searchItem({ auth, request, response }: HttpContext) {
     const query = request.input('query')
 
     const result = await this.tmdbService.searchItem(query)
-    return response.ok({ result })
+
+    const userHistory = await WatchedItem.query()
+      .where('user_id', auth.user!.id)
+      .select('id_tmdb', 'id')
+
+    const historyMap = new Map(userHistory.map((item) => [item.idTmdb, item.id]))
+
+    const resultsWithSeen = result.results.map((item) => {
+      const idTmdbStr = item.id.toString()
+      const watchedId = historyMap.get(idTmdbStr) || null
+      return {
+        ...item,
+        seen: watchedId !== null,
+        watched_id: watchedId,
+      }
+    })
+
+    return response.ok({ ...result, results: resultsWithSeen })
   }
 }
